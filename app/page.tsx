b6a36ea1abafa88db12c039b5728490196ff57db
vansh <vanshchoudhary2744@gmail.com>
2025-01-03 12:01:11 +0530

    changed button tag to Link tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import { redirect } from "next/navigation";
+import Link from "next/link";
 
 export default async function Home() {
 
   return (
     <div>
       <h1>Home</h1>
-      <button className="bg-blue-500 py-2 px-4" onClick={() => redirect("/dashboard")}>dashboard</button>
+      <Link className="bg-blue-500 py-2 px-4" href="/dashboard">dashboard</Link>
     </div>
   );
 }
