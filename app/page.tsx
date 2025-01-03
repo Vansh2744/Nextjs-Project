2771082417c22d4b5685ea43457d2c467fc4ba19
vansh <vanshchoudhary2744@gmail.com>
2025-01-03 12:06:37 +0530

    changed Link tag to a tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Link from "next/link";
-
 export default async function Home() {
 
   return (
     <div>
       <h1>Home</h1>
-      <Link className="bg-blue-500 py-2 px-4" href="/dashboard">dashboard</Link>
+      <a className="bg-blue-500 py-2 px-4" href="/dashboard">dashboard</a>
     </div>
   );
 }
