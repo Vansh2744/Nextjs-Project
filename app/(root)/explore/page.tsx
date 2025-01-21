d06ee44b2c633092fe442c74e9798a836f4070a3
Vansh <vanshchoudhary2744@gmail.com>
2025-01-21 22:08:56 +0530

    solved all errors

--- app/(root)/explore/page.tsx
+++ app/(root)/explore/page.tsx
@@ -34,6 +34,14 @@ function Explore() {
 
     fetchVideos();
   }, []);
+
+  if (videos.length === 0) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="w-16 h-16 border-4 border-orange-600 border-t-transparent rounded-full animate-spin"></div>
+      </div>
+    );
+  }
   return (
     <div>
       <div>
--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 interface Video {
   url: string;
@@ -19,46 +20,48 @@ function ProfilePage() {
   const videos = searchParams.get("videos");
   const parsedVideos: Video[] = videos ? JSON.parse(videos) : [];
   return (
-    <div className="px-40">
-      <div className="mt-10 flex">
-        <img
-          src={
-            image ||
-            "https://res.cloudinary.com/djkqpnoks/image/upload/v1734583859/pek6axvgyifougyfqzmc.jpg"
-          }
-          width={200}
-          height={200}
-          alt="User profile"
-          className="w-[200px] h-[200px] rounded-full shadow-lg shadow-orange-600"
-        />
-        <div className="ml-10 flex flex-col gap-3 justify-center text-xl text-orange-600">
-          <h1 className="text-3xl font-bold">{username}</h1>
-          <h1>
-            {firstname} {lastname}
-          </h1>
+    <Suspense fallback={<p>Loading...</p>}>
+      <div className="px-40">
+        <div className="mt-10 flex">
+          <img
+            src={
+              image ||
+              "https://res.cloudinary.com/djkqpnoks/image/upload/v1734583859/pek6axvgyifougyfqzmc.jpg"
+            }
+            width={200}
+            height={200}
+            alt="User profile"
+            className="w-[200px] h-[200px] rounded-full shadow-lg shadow-orange-600"
+          />
+          <div className="ml-10 flex flex-col gap-3 justify-center text-xl text-orange-600">
+            <h1 className="text-3xl font-bold">{username}</h1>
+            <h1>
+              {firstname} {lastname}
+            </h1>
+          </div>
+        </div>
+        <hr className="mt-10" />
+        <div className="my-20 flex gap-10 flex-wrap">
+          {parsedVideos?.map((video, index) => (
+            <div key={index} className="shadow-lg shadow-gray-600 ">
+              <video
+                controls
+                className="w-[355px] h-[200px]"
+                poster={video.thumbnail}
+              >
+                <source src={video.url} type="video/mp4" />
+              </video>
+              <div className="p-5 flex flex-col gap-3">
+                <h1 className="font-extrabold text-lg text-gray-300">
+                  {video.title}
+                </h1>
+                <p className="text-gray-600">{video.description}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-      <hr className="mt-10" />
-      <div className="my-20 flex gap-10 flex-wrap">
-        {parsedVideos?.map((video, index) => (
-          <div key={index} className="shadow-lg shadow-gray-600 ">
-            <video
-              controls
-              className="w-[355px] h-[200px]"
-              poster={video.thumbnail}
-            >
-              <source src={video.url} type="video/mp4" />
-            </video>
-            <div className="p-5 flex flex-col gap-3">
-              <h1 className="font-extrabold text-lg text-gray-300">
-                {video.title}
-              </h1>
-              <p className="text-gray-600">{video.description}</p>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+    </Suspense>
   );
 }
 
