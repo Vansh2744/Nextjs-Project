21c3bf88067255c9683a82dbe1723fd280648fe3
Vansh <vanshchoudhary2744@gmail.com>
2025-01-21 22:14:35 +0530

    solved all errors

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -11,7 +11,7 @@ interface Video {
   description: string;
 }
 
-function ProfilePage() {
+function ProfileContent() {
   const searchParams = useSearchParams();
   const username = searchParams.get("username");
   const firstname = searchParams.get("firstname");
@@ -20,47 +20,53 @@ function ProfilePage() {
   const videos = searchParams.get("videos");
   const parsedVideos: Video[] = videos ? JSON.parse(videos) : [];
   return (
-    <Suspense fallback={<p>Loading...</p>}>
-      <div className="px-40">
-        <div className="mt-10 flex">
-          <img
-            src={
-              image ||
-              "https://res.cloudinary.com/djkqpnoks/image/upload/v1734583859/pek6axvgyifougyfqzmc.jpg"
-            }
-            width={200}
-            height={200}
-            alt="User profile"
-            className="w-[200px] h-[200px] rounded-full shadow-lg shadow-orange-600"
-          />
-          <div className="ml-10 flex flex-col gap-3 justify-center text-xl text-orange-600">
-            <h1 className="text-3xl font-bold">{username}</h1>
-            <h1>
-              {firstname} {lastname}
-            </h1>
-          </div>
-        </div>
-        <hr className="mt-10" />
-        <div className="my-20 flex gap-10 flex-wrap">
-          {parsedVideos?.map((video, index) => (
-            <div key={index} className="shadow-lg shadow-gray-600 ">
-              <video
-                controls
-                className="w-[355px] h-[200px]"
-                poster={video.thumbnail}
-              >
-                <source src={video.url} type="video/mp4" />
-              </video>
-              <div className="p-5 flex flex-col gap-3">
-                <h1 className="font-extrabold text-lg text-gray-300">
-                  {video.title}
-                </h1>
-                <p className="text-gray-600">{video.description}</p>
-              </div>
-            </div>
-          ))}
+    <div className="px-40">
+      <div className="mt-10 flex">
+        <img
+          src={
+            image ||
+            "https://res.cloudinary.com/djkqpnoks/image/upload/v1734583859/pek6axvgyifougyfqzmc.jpg"
+          }
+          width={200}
+          height={200}
+          alt="User profile"
+          className="w-[200px] h-[200px] rounded-full shadow-lg shadow-orange-600"
+        />
+        <div className="ml-10 flex flex-col gap-3 justify-center text-xl text-orange-600">
+          <h1 className="text-3xl font-bold">{username}</h1>
+          <h1>
+            {firstname} {lastname}
+          </h1>
         </div>
       </div>
+      <hr className="mt-10" />
+      <div className="my-20 flex gap-10 flex-wrap">
+        {parsedVideos?.map((video, index) => (
+          <div key={index} className="shadow-lg shadow-gray-600 ">
+            <video
+              controls
+              className="w-[355px] h-[200px]"
+              poster={video.thumbnail}
+            >
+              <source src={video.url} type="video/mp4" />
+            </video>
+            <div className="p-5 flex flex-col gap-3">
+              <h1 className="font-extrabold text-lg text-gray-300">
+                {video.title}
+              </h1>
+              <p className="text-gray-600">{video.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function ProfilePage() {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <ProfileContent />
     </Suspense>
   );
 }
