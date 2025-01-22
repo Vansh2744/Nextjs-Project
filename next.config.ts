152258fad8c3c2c539976c6c5b793f9f3cc04c7e
Vansh <vanshchoudhary2744@gmail.com>
2025-01-22 12:20:01 +0530

    solved all errors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: ["img.clerk.com", "res.cloudinary.com"], // Add img.clerk.com to the list
+  },
 };
 
 export default nextConfig;
